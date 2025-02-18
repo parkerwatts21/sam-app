272d83bdb4d25007e5a7f4852a9f3d70b2ebad76
Parker Watts <parkerwatts@mac.lan>
2025-02-18 14:29:42 -0700

    last: commit

--- hello-world/app.mjs
+++ hello-world/app.mjs
@@ -1,3 +1,5 @@
+// Parker Watts 
+
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
@@ -12,13 +14,20 @@
  */
 
 export const lambdaHandler = async (event, context) => {
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "I'm using canary deployments",
-      })
-    };
+  // throw new Error("This will cause a deployment rollback")
+  console.log("Change deployed with SAM Accelerate")
+  try {
+      response = {
+          "statusCode": 200,
+          "body": JSON.stringify({
+              message: "I'm using canary deployments",
+          })
+      }
+  } catch (err) {
+      console.log(err);
+      return err;
+  }
 
-    return response;
-  };
+  return response
+};
   
